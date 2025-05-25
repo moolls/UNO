c419187aa3bb3b2dd3140f8ed417c47c36c7f01f
Emily Chambers <115665888+moolls@users.noreply.github.com>
2025-05-25 22:10:07 +0200

    Delete UNOGame-PG4 directory (gammal)

--- UNOGame-PG4/UNOGame-PG4/IUnoCard.cs
+++
@@ -1,9 +0,0 @@
-public interface IUnoCard
-{
-   
-    public void DisplayCard();
-
-    public CardColor GetColor();
-    public CardValue GetValue();
-
-}
--- UNOGame-PG4/UNOGame-PG4/Program.cs
+++
@@ -1,1 +0,0 @@
-﻿Game game = new Game();
